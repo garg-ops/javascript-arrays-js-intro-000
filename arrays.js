687a5ce6540464f2c7126eb95e79f6a291432007
Nilesh Garg <nileshgarg0023@gmail.com>
2021-03-02 04:23:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+chocolateBars.push('Five Starts')
+chocolateBars.unshift('snaker')
+
+
+function addElementToBeginningOfArray(a,b) {
+
+
+  var a = []
+
+  a.push(b)
+
+return a
+
+}
+
+function destructivelyAddElementToBeginningOfArray () {
+
+}
